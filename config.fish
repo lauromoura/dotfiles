b55fe1781d03bfa506f40352577c501f865ce81d
Lauro Moura <lmoura@igalia.com>
2022-08-09 22:19:29 -0300

    Add some fish aliases

--- config.fish
+++ config.fish
@@ -2,7 +2,13 @@ alias t=todo-txt
 alias wat=watson
 alias wtb=wk-test-baselines
 alias wtl=wk-test-lookup
+alias exa="exa --icons"
 set -xg PATH /home/lauro/bin $PATH
 set -xg PATH /home/lauro/.local/bin $PATH
 set -xg PATH /home/lauro/.cargo/bin $PATH
 set -xg PATH /home/lauro/go/bin $PATH
+set -xg PATH /opt/btop/bin $PATH
+
+# if status is-interactive
+# 	eval (zellij setup --generate-auto-start fish | string collect)
+# end
